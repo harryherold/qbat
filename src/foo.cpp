7b9bf6094fa598923c0440a03a588fa0dc1f419c
Christian Herold <harryherold@googlemail.com>
2017-09-29 10:37:24 +0200

    Connect to both services

--- src/foo.cpp
+++ src/foo.cpp
@@ -4,18 +4,18 @@ Foo::Foo (QObject * parent)
 :QObject(parent)
 {
     QDBusConnection dbus = QDBusConnection::systemBus ();
-    // bool ret = dbus.connect("org.freedesktop.UPower",
-    //                         "/org/freedesktop/UPower",
-    //                         "org.freedesktop.DBus.Properties",
-    //                         "PropertiesChanged",
-    //                         this,
-    //                         SLOT(updateProperties(QString, QMap<QString, QVariant>,QStringList)));
-    // if (ret)
-    //     qDebug () << "Connected";
-    // else
-    //     qDebug () << "Not connected";
-
     bool ret = dbus.connect("org.freedesktop.UPower",
+                            "/org/freedesktop/UPower",
+                            "org.freedesktop.DBus.Properties",
+                            "PropertiesChanged",
+                            this,
+                            SLOT(updateProperties(QString, QMap<QString, QVariant>,QStringList)));
+    if (ret)
+        qDebug () << "Connected";
+    else
+        qDebug () << "Not connected";
+
+    ret = dbus.connect("org.freedesktop.UPower",
                        "/org/freedesktop/UPower/devices/battery_BAT1",
                        "org.freedesktop.DBus.Properties",
                        "PropertiesChanged",
